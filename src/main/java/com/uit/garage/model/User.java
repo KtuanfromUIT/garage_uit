e925d13ddfae39631c0e67ed85fc0b8572b85090
KtuanfromUIT <157972313+KtuanfromUIT@users.noreply.github.com>
2025-06-21 18:25:12 +0700

    Create User.java

---
+++ src/main/java/com/uit/garage/model/User.java
@@ -0,0 +1,28 @@
+
+package com.uit.garage.model;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private Role role;
+
+    
+    public User(int id, String username, String password, Role role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters (không có setter cho password để đảm bảo bảo mật)
+    public int getId() { return id; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public Role getRole() { return role; }
+
+    // Phương thức kiểm tra quyền
+    public boolean hasRole(Role roleToCheck) {
+        return this.role == roleToCheck;
+    }
+}
